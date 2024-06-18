88a8ac2515db2cba754bd03af0c0f853d13146cb
Pierre Romera <hello@pirhoo.com>
2024-06-18 16:48:55 +0000

    test: split script checks

--- tests/e2e/default.js
+++ tests/e2e/default.js
@@ -5,10 +5,13 @@ module.exports = {
     browser.url('http://localhost:8008')
     // The plugin must be added
     browser.expect.element(`script[src="/plugins/package/${main}"]`).to.be.present
+  },
+  'it configures google analytics': function(browser) {
+    browser.url('http://localhost:8008')
     // Wait for the app to be started
     browser.waitForElementVisible('.landing')
     browser.assert.titleContains('Datashare')
     // A script tag was created for Google Analytics
     browser.expect.element(`script[src="https://www.google-analytics.com/analytics.js"]`).to.be.present
   }
-};
+}
