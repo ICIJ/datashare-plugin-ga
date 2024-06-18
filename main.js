9f06e5a454a094191df46913f561552764ded95a
Pierre Romera <hello@pirhoo.com>
2024-06-18 16:44:56 +0000

    chore: replace vue-analytics by vue-gtag-next

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import VueAnalytics from 'vue-analytics'
+import VueGtag, { trackRouter } from "vue-gtag-next"
 
 document.addEventListener('datashare:ready', async ({ detail: { core } }) => {
   const id = core.config.get('gaId', null)
   // No need to add tracker if no id is provided
   if (id !== null) {
-    core.use(VueAnalytics, { id, router: core.router })
+    // Add the Google Analytics tracker
+    core.use(VueGtag, { property: { id } })
+    // Wait for the core to be ready before tracking the router
+    await core.ready
+    trackRouter(core.router)
   }
 }, false)
